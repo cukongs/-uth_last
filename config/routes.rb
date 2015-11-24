71e37239f9d943d3800382a557ed660e447e7ea2
Bengsiswanto Hendrawan <bengsis@dwp.co.id>
2015-11-24 13:33:49 +0700

    add homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  use_doorkeeper
-  # resources :users
 
-  # root 'users#index'
+  use_doorkeeper
 
   get '/me' => 'users#me'
+
+  root 'home#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
